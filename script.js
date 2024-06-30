8ee8c4d86879c925cdbad0ece7e59b1a65508aad
Jubair1996 <abujubair1996@gmail.com>
2024-06-30 11:06:00 +0600

    select elements & assign them to variables

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+ // select elements & assign them to variables
+ let newTask = document.querySelector('#new-task');
+ let form = document.querySelector('form');
+ let todoUl = document.querySelector('#items');
+ let completeUl = document.querySelector('.complete-list ul');
+
+
